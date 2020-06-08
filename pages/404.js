81e5c979a44f33d14c1fcea8b4ea9e2e14a48def
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-08 15:01:26 +0200

    error page impl

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import useTranslation from "../hooks/useTranslation";
+import Layout from "../components/layout/layout";
+import {Col, Container, Row} from "react-bootstrap";
+import Link from "next/link";
 
-const Custom404 = () => (
-    <h1>404 - Page Not Found</h1>
-);
+const Custom404 = () => {
+  const { t, locale } = useTranslation();
 
+  return (
+      <Layout banner={{
+        caption: t('notfound-page-caption')
+      }}>
+        <Container>
+          <Row>
+            <Col md={12}>
+              {t('notfound-page-message')}
+            </Col>
+            <Link href={`/${locale}/`} passHref>
+              <a className="btn btn-primary btn-lg" >
+                {t('notfound-page-homepage-button')}
+              </a>
+            </Link>
+          </Row>
+        </Container>
+      </Layout>
+  )
+};
+
+//TODO: custom404 cannot be used with HOC now (getInitialProps problem), withLocale is needed to translate to other languages
 export default Custom404;
