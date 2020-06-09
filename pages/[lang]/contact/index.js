509dd3595d12e684c1cb4ad36ef24afb06d89a2e
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-09 07:46:42 +0200

    added contact index page

---
+++ pages/[lang]/contact/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import withLocale from '../../../hocs/withLocale';
+import useTranslation from '../../../hooks/useTranslation';
+import Layout from '../../../components/layout/layout';
+import {Col, Container, Row} from 'react-bootstrap';
+import classes from './index.module.css';
+
+const contact = () => {
+  const { t } = useTranslation();
+
+  return (
+      <Layout banner={{
+        caption: t('contact-page-caption')
+      }}>
+        <Container>
+          <Row>
+            <Col md={12}>
+              <h4>
+                {t('contact-address-title-1')}
+              </h4>
+              <address>
+                <span className={classes.addressFirstLine}>
+                  Hypertherm Inc.
+                </span><br />
+                P.O. Box 5010<br />
+                Hanover, NH 03755-5010
+              </address>
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col md={12}>
+              <h4>
+                {t('contact-address-title-2')}
+              </h4>
+              <address>
+                <span className={classes.addressFirstLine}>
+                  Hypertherm Robotic Software Inc.
+                </span><br />
+                5929 Rte Transcanadienne, suite 330<br />
+                St. Laurent (QC)<br />
+                Canada H4T 1Z6
+              </address>
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col md={12}>
+              <address>
+                Tel : +1 514-225-2206<br />
+                Fax : +1 514-221-3661<br />
+              </address>
+            </Col>
+          </Row>
+          <h2>
+            {t('inforeq-page-caption')}
+          </h2>
+
+          {/*TODO: add contactForm, gaEventName: '/contact-page-request-form-submitted', requestingPage: 'contact-page',*/}
+
+          <hr />
+          <Row>
+            <Col md={12} sm={12} xs={12}>
+              <h4>
+                {t('contact-reseller-inq')}
+              </h4>
+              <p>
+                {t('contact-reseller-para')}
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
+  );
+};
+
+export default withLocale(contact);
