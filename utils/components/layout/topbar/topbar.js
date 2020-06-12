425f00619f5b7c8fd4300da3dda499ac0f4a1155
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-12 15:08:09 +0200

    fixed topbar by using the old solution

--- utils/components/layout/topbar/topbar.js
+++ utils/components/layout/topbar/topbar.js
@@ -1,19 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import {
-  Container,
-  Nav,
-  Navbar, NavDropdown
+  NavDropdown
 } from 'react-bootstrap';
 import useTranslation from "../../../hooks/useTranslation";
 import {languageNames} from '../../../../lib/translations/config';
 import Link from 'next/link';
+import { FaBars} from "react-icons/fa";
+
+//TODO: copied from the old codebase, refactor
 
 const topbar = () => {
   const { locale, t } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isTopbarFixed, setIsTopbarFixed] = useState(false);
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    }
   });
 
   const handleScroll = () => {
@@ -23,51 +32,97 @@ const topbar = () => {
     }
   }
 
-  return (
-      <div className={'topbar ' + (isTopbarFixed ? 'topbarFixed' : '')}>
-        <Container>
-          <Link href={`/${locale}/contact/live-demo-request`} passHref>
+  const handleResize = () => {
+    setIsMobileScreen(window.innerWidth <= 991);
+  }
+
+  if (isMobileScreen) {
+    return (
+        <div className={'topbar topbarMobile ' + (isTopbarFixed ? 'topbarFixed' : '')}>
+          <div className="container">
             <a
                 className={'btn btn-primary navbar-cta navbar-cta-first ' +
-                (isTopbarFixed ? 'navbar-cta-fixed' : '')}>
+                (isTopbarFixed ? 'navbar-cta-fixed' : '')}
+                href="contact/live-demo-request">
               {t('topbar-live-demo')}
             </a>
-          </Link>
-          <Link href={`/${locale}/contact/contact-me-request`} passHref>
             <a
                 className={'btn btn-primary navbar-cta ' +
-                (isTopbarFixed ? 'navbar-cta-fixed' : '')}>
-              {t('topbar-contact-me')}
+                (isTopbarFixed ? 'navbar-cta-fixed' : '')}
+                href="contact/contact-me-request">
+              {t('topbar-contact-me',)}
             </a>
-          </Link>
-          <Navbar expand='lg' variant='dark'>
-            <Navbar.Toggle aria-controls='topbar-navbar-nav' />
-            <Navbar.Collapse id='topbar-navbar-nav'>
-              <Nav className='mr-auto'>
-                <Link href={`/${locale}/contact`} passHref>
-                  <Nav.Link>{t('contact-page-caption')}</Nav.Link>
-                </Link>
-                <Link href={`/${locale}/newsroom`} passHref>
-                  <Nav.Link>{t('blog-page-caption')}</Nav.Link>
-                </Link>
-                <Nav.Link href="https://robotmaster.atlassian.net/servicedesk/customer/portals"
-                          rel='noreferrer noopener'
-                          target="_blank">{t('topbar-support')}</Nav.Link>
-                <NavDropdown id="language-chooser" title={languageNames[locale]}>
-                  {Object.keys(languageNames).map(key => {
-                    if (languageNames[key]) {
-                      return (
-                          <NavDropdown.Item href={`/${key}`}>
-                            {languageNames[key]}
-                          </NavDropdown.Item>
-                      );
-                    }
-                  })}
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Container>
+            <FaBars style={{fontSize: '25px', cursor: 'pointer',}}
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}/>
+          </div>
+          <div
+              className={'topbarMenu ' + (isMenuOpen ? 'topbarMenu--open ' : 'topbarMenu--closed ') +
+              (isTopbarFixed ? 'topbarMenu--lower' : 'topbarMenu--upper')}>
+            <a href="contact">
+              {t('contact-page-caption')}
+            </a>
+            <a href="newsroom">
+              {t('blog-page-caption')}
+            </a>
+            <a
+                href="https://robotmaster.atlassian.net/servicedesk/customer/portals"
+                target="_blank">
+              {t('topbar-support')}
+            </a>
+            <NavDropdown id="language-chooser" title={languageNames[locale]}>
+              {Object.keys(languageNames).map(key => {
+                if (languageNames[key]) {
+                  return (
+                      <NavDropdown.Item href={`/${key}`}>
+                        {languageNames[key]}
+                      </NavDropdown.Item>
+                  );
+                }
+              })}
+            </NavDropdown>
+          </div>
+        </div>
+    );
+  }
+
+  return (
+      <div className={'topbar ' + (isTopbarFixed ? 'topbarFixed' : '')}>
+        <div className="container">
+          <a
+              className={'btn btn-primary navbar-cta navbar-cta-first ' +
+              (isTopbarFixed ? 'navbar-cta-fixed' : '')}
+              href="contact/live-demo-request">
+            {t('topbar-live-demo')}
+          </a>
+          <a
+              className={'btn btn-primary navbar-cta ' +
+              (isTopbarFixed ? 'navbar-cta-fixed' : '')}
+              href="contact/contact-me-request">
+            {t('topbar-contact-me')}
+          </a>
+          <a href="contact">
+            {t('contact-page-caption')}
+          </a>
+          <a href="newsroom">
+            {t('blog-page-caption')}
+          </a>
+          <a
+              href="https://robotmaster.atlassian.net/servicedesk/customer/portals"
+              target="_blank">
+            {t('topbar-support')}
+          </a>
+          <NavDropdown id="language-chooser" title={languageNames[locale]}>
+            {Object.keys(languageNames).map(key => {
+              if (languageNames[key]) {
+                return (
+                    <NavDropdown.Item href={`/${key}`}>
+                      {languageNames[key]}
+                    </NavDropdown.Item>
+                );
+              }
+            })}
+          </NavDropdown>
+        </div>
       </div>
   );
 };
