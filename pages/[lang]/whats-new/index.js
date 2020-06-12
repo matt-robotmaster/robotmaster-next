1469121186ae1a060950cc40c7a23aa4ed0aa608
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-12 10:26:23 +0200

    what's new page (v6.6)

--- pages/[lang]/whats-new/index.js
+++ pages/[lang]/whats-new/index.js
@@ -11,131 +11,46 @@ const whatsNew = () => {
         caption: t('whats-new-page-caption')
       }}>
         <h1 className="page-header">
-          {t('whats-new-v65-title')}
+          {t('whats-new-v66-title')}
         </h1>
         <p>
-          {t('whats-new-v65-para')}
+          {t('whats-new-v66-para')}
         </p>
         <h3>
-          {t('whats-new-v65-heading-1')}
+          {t('whats-new-v66-heading-1')}
         </h3>
         <ul>
           <li>
-            {t('whats-new-v65-list-1-item-1')}
+            {t('whats-new-v66-list-1-item-1')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-2')}
+            {t('whats-new-v66-list-1-item-2')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-3')}
+            {t('whats-new-v66-list-1-item-3')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-4')}
+            {t('whats-new-v66-list-1-item-4')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-5')}
+            {t('whats-new-v66-list-1-item-5')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-6')}
+            {t('whats-new-v66-list-1-item-6')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-7')}
+            {t('whats-new-v66-list-1-item-7')}
           </li>
           <li>
-            {t('whats-new-v65-list-1-item-8')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-1-item-9')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-1-item-10')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-1-item-11')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-1-item-12')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-1-item-13')}
+            {t('whats-new-v66-list-1-item-8')}
           </li>
         </ul>
         <h3>
-          {t('whats-new-v65-heading-2')}
+          {t('whats-new-v66-heading-2')}
         </h3>
         <ul>
           <li>
-            {t('whats-new-v65-list-2-item-1')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-2-item-2')}
-            <ul>
-              <li>
-                {t('whats-new-v65-list-2-item-2-subitem-1')}
-              </li>
-              <li>
-                {t('whats-new-v65-list-2-item-2-subitem-2')}
-              </li>
-            </ul>
-          </li>
-          <li>
-            {t('whats-new-v65-list-2-item-3')}
-            <ul>
-              <li>
-                {t('whats-new-v65-list-2-item-3-subitem-1')}
-              </li>
-              <li>
-                {t('whats-new-v65-list-2-item-3-subitem-2')}
-              </li>
-            </ul>
-          </li>
-          <li>
-            {t('whats-new-v65-list-2-item-4')}
-            <ul>
-              <li>
-                {t('whats-new-v65-list-2-item-4-subitem-1')}
-              </li>
-            </ul>
-          </li>
-          <li>
-            {t('whats-new-v65-list-2-item-5')}
-            <ul>
-              <li>
-                {t('whats-new-v65-list-2-item-5-subitem-1')}
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <h3>
-          {t('whats-new-v65-heading-3')}
-        </h3>
-        <ul>
-          <li>
-            {t('whats-new-v65-list-3-item-1')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-2')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-3')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-4')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-5')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-6')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-7')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-8')}
-          </li>
-          <li>
-            {t('whats-new-v65-list-3-item-9')}
+            {t('whats-new-v66-list-2-item-1')}
           </li>
         </ul>
       </Layout>
