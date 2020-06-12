f91a9d36de4b15173297c030e4360d430580ce7b
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-12 10:33:28 +0200

    added partners page

---
+++ pages/[lang]/partners/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Layout from '../../../utils/components/layout/layout';
+import useTranslation from "../../../utils/hooks/useTranslation";
+import withLocale from "../../../utils/hocs/withLocale";
+import {Col} from "react-bootstrap";
+import classes from './index.module.css';
+
+const logos = [
+  '/img/partners/abb.gif',
+  '/img/partners/fanuc.jpg',
+  '/img/partners/kuka.gif',
+  '/img/partners/yaskawa.jpg',
+  '/img/partners/staubli.gif',
+  '/img/partners/comau-robotics.png',
+  '/img/partners/denso.jpg',
+  '/img/partners/mitsubishi.jpg',
+  '/img/partners/hyundai.jpg',
+  '/img/partners/kawasaki.jpg',
+  '/img/partners/otc.jpg',
+  '/img/partners/ur.jpg',
+  '/img/partners/panasonic.png',
+  '/img/partners/reis-robotics.png',
+  '/img/partners/epson-robotics.png',
+  '/img/partners/nachi.png',
+];
+
+const whatsNew = () => {
+  const { t } = useTranslation();
+
+  return (
+      <Layout banner={{
+        caption: t('partners-page-caption')
+      }}>
+        <h1 id="robot">
+          {t('partners-heading')}
+        </h1>
+        <h4>
+          {t('partners-sub-heading')}
+        </h4>
+        <div className="row">
+          {logos.map(logo => (
+              <Col md={3} sm={3} xs={4} className={classes.partnerLogo} key={logo}>
+                <img src={logo} />
+              </Col>
+          ))}
+        </div>
+      </Layout>
+  );
+};
+
+export default withLocale(whatsNew);
