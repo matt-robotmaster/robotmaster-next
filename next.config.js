ab9ef38ac0e93a440f14fd6c70c122d0aeaa6826
Szabó Bence <szabo.bence.tat@gmail.com>
2020-06-16 09:56:09 +0200

    added redirections

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  env: {
+    PORT: 8080,
+    AWS_ACCESS_KEY_ID: '0',
+    AWS_SECRET_KEY: '0',
+    AWS_REGION: 'us-east-1',
+  },
+
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
@@ -9,10 +16,43 @@ module.exports = {
 
     return config
   },
-  env: {
-    PORT: 8080,
-    AWS_ACCESS_KEY_ID: '0',
-    AWS_SECRET_KEY: '0',
-    AWS_REGION: 'us-east-1',
-  }
+  experimental: {
+    async redirects() {
+      return [
+        {
+          source: "/:lang/products/whats-new-in-v5",
+          destination: "/products",
+          statusCode: 301
+        }, {
+          source: "/:lang/support",
+          destination: "https://support.robotmaster.com",
+          statusCode: 301
+        }, {
+          source: "/:lang/blog",
+          destination: "/:lang/newsroom",
+          statusCode: 301
+        }, {
+          source: "/:lang/products/whats-new-in-v6",
+          destination: "/:lang/products#whats-new-v6",
+          statusCode: 301
+        }, {
+          source: "/:lang/why-robotmaster/evolution-chart",
+          destination: "/:lang/why-robotmaster#evolution-of-robot-programming",
+          statusCode: 301
+        }, {
+          source: "/:lang/why-robotmaster/benefits",
+          destination: "/:lang/why-robotmaster#solving-programming-challenges",
+          statusCode: 301
+        }, {
+          source: "/:lang/resellers",
+          destination: "/:lang/contact",
+          statusCode: 301
+        }, {
+          source: "/:lang/v6-3",
+          destination: "/:lang/whats-new",
+          statusCode: 301
+        }
+      ];
+    }
+  },
 }
